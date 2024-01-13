c1dd949f0bf0896c46597275ce8cc3c9a52c7f9b
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-01-13 17:31:47 +0530

    Connected components

--- cooccurrence.py
+++ cooccurrence.py
@@ -23,11 +23,11 @@ except:
     while i < len(w):
         if w[i][0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" and w[i].lower() not in stopwords.words('english'):
             name = []
-            pos = i
+            beg = i
             while w[i][0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ'" and w[i] != "'s" or w[i][:2] == "u-":
                 name += [w[i]]
                 i += 1
-            occurrences[tuple(name)].append(pos)
+            occurrences[tuple(name)].append((beg, i))
         i += 1
 
     occurrences = {tuple(word for word in n if word.lower() not in words.words()) : occurrences[n] for n in tqdm(occurrences.keys(), desc='Filtering English words')}
@@ -37,6 +37,7 @@ except:
         pickle.dump(occurrences, handle, protocol=pickle.HIGHEST_PROTOCOL)
 names = list(occurrences.keys())
 
+# For all pairs of words, find the number of times they occur within W words of each other.
 W = 5
 cooccurrence = {}
 for i in tqdm(range(len(names)), desc="Extracting cooccurrences"):
@@ -44,15 +45,23 @@ for i in tqdm(range(len(names)), desc="Extracting cooccurrences"):
     positions1 = occurrences[name1]
     for name2 in names[:i] + names[i+1:]:
         positions2 = occurrences[name2]
-        distances = [abs(p1 - p2) for p1 in positions1 for p2 in positions2]
+        distances = [min(abs(b1-e2), abs(b2-e1)) for b1, e1 in positions1 for b2, e2 in positions2]
         cooccurrence[(name1, name2)] = sum(1 for d in distances if d <= W)
 
+# Construct an unweighted graph with 
 G = nx.Graph()
 G.add_nodes_from(names)
 for (n1, n2) in cooccurrence:
-    if cooccurrence[(n1, n2)] > 0: # and n1 in mfn and n2 in mfn:
+    if cooccurrence[(n1, n2)] > 0:
         G.add_edge(n1, n2)
 
-nx.draw(G, with_labels=True)
-plt.show()
-plt.savefig("without_weights.png")
+# Remove nodes without edges
+for n in names:
+    if len(G.edges(n)) == 0:
+        G.remove_node(n)
+
+components = [G.subgraph(c).copy() for c in sorted(nx.connected_components(G), key=len, reverse=True)]
+
+for component in components:
+    nx.draw(component, with_labels=True)
+    plt.show()
